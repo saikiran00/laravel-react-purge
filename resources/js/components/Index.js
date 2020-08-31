61d4c99154ccb09b931a4ffd61019ea263e63191
saikiran00 <saikiran@betalectic.com>
2020-08-31 22:20:50 +0530

    production build

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -10,13 +10,16 @@ function Index() {
     };
     return (
         <div className="mx-48">
-            <span className="inline-flex rounded-md shadow-sm">
+            <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+                This is Inside React
+            </h2>
+            <span className="inline-flex rounded-md shadow-sm mt-4">
                 <button
                     onClick={() => setShow(true)}
                     type="button"
                     className="inline-flex items-center px-3 py-2 border border-transparent text-xs leading-4 font-medium rounded text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
                 >
-                    Open
+                    Click Here to Open
                 </button>
 
                 {show && <Modal closeModal={closeModal} />}
